71383e6075646c9921a23131da8329c5edfbca6b
Yosef Melamed <yosefmelamed112@gmail.com>
2022-11-14 17:25:52 -0700

    fixingMobileDesign

--- src/profile-components/About/About.js
+++ src/profile-components/About/About.js
@@ -1,15 +1,47 @@
 export default function About() {
   return (
     <>
-      <div className="text-center">
-        <div id="construction"></div>
-        <h4 className="p-4 my-5">
-          This page is under construction! Please bear with us in the meantime!
-        </h4>
-        <a href="/" id="goback">
-          Click Here to Go Back!
-        </a>
+      <div
+        id="carouselExampleSlidesOnly"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="./images/mypicture2.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="./images/Thinkful BNB.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="./images/We Love Movies.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
       </div>
     </>
   );
+}
+
+{
+  /* <div className="text-center">
+<div id="construction"></div>
+<h4 className="p-4 my-5">
+  This page is under construction! Please bear with us in the meantime!
+</h4>
+<a href="/" id="goback">
+  Click Here to Go Back!
+</a>
+</div> */
 }
